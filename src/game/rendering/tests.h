b3557cf6f46d7af04efd282baa3795d4f4801193
Matt Wamboldt <mattwamboldt@hotmail.com>
2015-09-09 16:24:16 -0300

    Initial commit
    - Added basic vector3d and 4d classes
    - Starting to try out some line drawing algorithms

---
+++ src/game/rendering/tests.h
@@ -0,0 +1,8 @@
+#ifndef RENDERING_TESTS_H
+#define RENDERING_TESTS_H
+
+#include <SDL/SDL.h>
+
+void Draw(SDL_Surface* screen);
+
+#endif
---
+++ src/game/rendering/vector3.cpp
@@ -0,0 +1,35 @@
+#include "vector3.h"
+#include "vector4.h"
+
+#include <math.h>
+
+Vector3::Vector3(const Vector4& v) :
+    x(v.x), y(v.y), z(v.z)
+{}
+
+float Vector3::Length()
+{
+    return sqrt(x * x + y * y + z * z);
+}
+
+void Vector3::Normalize()
+{
+    float length = Length();
+    x /= length;
+    y /= length;
+    z /= length;
+}
+
+Vector3 Vector3::Cross(const Vector3& v2)
+{
+    Vector3 resultant;
+    resultant.x = y * v2.z - v2.y * z;
+    resultant.y = z * v2.x - v2.z * x;
+    resultant.z = x * v2.y - v2.x * y;
+    return resultant;
+}
+
+float Vector3::Dot(const Vector3& v2)
+{
+    return x * v2.x + y * v2.y + z * v2.z;
+}
